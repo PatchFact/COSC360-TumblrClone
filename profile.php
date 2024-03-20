23c8d7971f2ef50add350e18c40085c4317cce54
Patch <patchfact@gmail.com>
2024-03-19 19:41:48 -0700

    navbar moved to new file

--- profile.php
+++ profile.php
@@ -9,16 +9,8 @@ require "head.php";
 
 <body>
     <form action="search.php" method="post" style="width: fit-content"></form>
-    <header>
-        <nav>
-            <ul>
-                <li><img src="images/AraLogo.png" /></li>
-                <li><a href="#">Home</a></li>
-                <li><a href="#">Profile</a></li>
-                <li><a href="#">New Post</a></li>
-            </ul>
-        </nav>
-    </header>
+    <?php require "navbar.php" ?>
+
 
     <div id="main">
         <article id="side-profile">
