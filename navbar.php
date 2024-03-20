d9c37886a20a25659aa666779447a3964575dbcb
Patch <patchfact@gmail.com>
2024-03-19 20:36:08 -0700

    rename makePost for convention

--- navbar.php
+++ navbar.php
@@ -4,7 +4,7 @@
             <li><img src="images/AraLogo.png" /></li>
             <li><a href="index.php">Home</a></li>
             <li><a href="profile.php">Profile</a></li>
-            <li><a href="make-post.php">New Post</a></li>
+            <li><a href="makePost.php">New Post</a></li>
         </ul>
     </nav>
 </header>
