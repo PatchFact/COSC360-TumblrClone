7d0c76427bb74daf11b5b07bdfdfaef3fb2d2451
Patch <patchfact@gmail.com>
2024-03-19 19:12:38 -0700

    removed head from pages for code reusal

---
+++ index.php
@@ -0,0 +1,39 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<?php
+$pageTitle = "Ara Home";
+require "head.php";
+?>
+
+
+<body>
+    <form action="search.php" method="post" style="width: fit-content"></form>
+    <header>
+        <nav>
+            <ul>
+                <li><img src="images/templogo.jpg" /></li>
+                <li><a href="#">Home</a></li>
+                <li><a href="#">Profile</a></li>
+                <li><a href="#">New Post</a></li>
+            </ul>
+        </nav>
+    </header>
+    <div id="main">
+        <article id="side-profile">
+            <a href="#" class="button">Log In</a>
+        </article>
+
+        <article id="feed">
+            <h1>This is Temporary</h1>
+        </article>
+
+        <article id="search-bar">
+            <input text="text" name="search" placeholder="Search posts">
+            <input text="text" name="filter" placeholder="Filter tags">
+        </article>
+        </form>
+    </div>
+</body>
+
+</html>
---
+++ make-post.php
@@ -0,0 +1,48 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<?php
+$pageTitle = "Ara Create Post";
+require "head.php";
+?>
+
+
+<body>
+    <form action="search.php" method="post" style="width: fit-content"></form>
+    <header>
+        <nav>
+            <ul>
+                <li><img src="images/templogo.jpg" /></li>
+                <li><a href="#">Home</a></li>
+                <li><a href="#">Profile</a></li>
+                <li><a href="#">New Post</a></li>
+            </ul>
+        </nav>
+    </header>
+
+    <div id="main">
+        <article id="side-profile">
+            <a href="#" class="button">Log In</a>
+        </article>
+
+        <article id="feed">
+            <div id="post-input">
+                <h2>Make Post</h2>
+
+                <!-- Image Post -->
+                <label for="image-post">Image:</label>
+                <input type="file" id="image-post" name="image-post" accept="image/*" />
+
+                <!-- Text Post -->
+                <label for="text-post">Text:</label>
+                <input type="text" id="text-post" name="text-post" placeholder="Enter post text here" />
+
+                <!-- Button to Save Changes -->
+                <button type="button" onclick="savePost()">Post</button>
+            </div>
+        </article>
+        </form>
+    </div>
+</body>
+
+</html>
