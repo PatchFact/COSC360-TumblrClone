d3d21b0e0f51c6d744711d28a2aff0b7847f9215
Jaidyn Gordon-Mason <jaidynrocks01@gmail.com>
2024-03-21 21:33:01 -0700

    blank

--- db.php
+++ db.php
@@ -1,11 +1,11 @@
 <?php
-$host = 'cosc360.ok.ubc.ca'; // Removed the trailing slash
+$host = 'cosc360.ok.ubc.ca'; 
 $dbname = 'db_18288647';
-$username = '18288647';
-$password = '18288647';
+$dbusername = '18288647';
+$dbpassword = '18288647';
 
 try {
-    $pdo = new PDO("mysql:host=$host;dbname=$dbname;charset=utf8mb4", $username, $password);
+    $pdo = new PDO("mysql:host=$host;dbname=$dbname;charset=utf8mb4", $dbusername, $dbpassword);
     $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
 } catch(PDOException $e) {
     echo "Connection failed: " . $e->getMessage();
