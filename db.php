5af42a165b4aa8d712a953d27e8b2ff95e89603d
Jaidyn Gordon-Mason <jaidynrocks01@gmail.com>
2024-03-21 20:32:56 -0700

    Last one

--- db.php
+++ db.php
@@ -1,15 +1,12 @@
 <?php
-$host = 'cosc360.ok.ubc.ca'; // Often this is 'localhost' or a specific server address or IP
-$dbname = 'db_18288647';
-$username = '18288647';
-$password = '18288647';
+require "dbDetails.php";
+
+$connectionString = "mysql:host=" . DBHOST . ";dbname=" . DBNAME;
+$dbuser = DBUSER;
+$dbpass = DBPASS;
 
 try {
-    $pdo = new PDO("mysql:host=$host;dbname=$dbname;charset=utf8mb4", $username, $password);
-    // Set the PDO error mode to exception
-    $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-    echo "Connected successfully"; 
-} catch(PDOException $e) {
-    echo "Connection failed: " . $e->getMessage();
+    $pdo = new PDO($connectionString, $dbuser, $dbpass);
+} catch (\PDOException $e) {
+    throw new \PDOException($e->getMessage(), (int)$e->getCode());
 }
-?>
