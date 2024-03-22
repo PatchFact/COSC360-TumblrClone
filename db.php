c2508d38bfb80fb7b6251dbf18cd31e192335207
Jaidyn Gordon-Mason <jaidynrocks01@gmail.com>
2024-03-21 20:51:38 -0700

    Getting there

--- db.php
+++ db.php
@@ -8,7 +8,6 @@ try {
     $pdo = new PDO("mysql:host=$dbhost;dbname=$dbname;charset=utf8mb4", $dbusername, $dbpassword);
     // Set the PDO error mode to exception
     $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-    echo "Connected successfully"; 
 } catch(PDOException $e) {
     echo "Connection failed: " . $e->getMessage();
 }
