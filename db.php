6381a0155c6821e2ef84349f1d2caea7b1cb2f73
Jaidyn Gordon-Mason <jaidynrocks01@gmail.com>
2024-03-21 20:26:20 -0700

    Trying to set up database

--- db.php
+++ db.php
@@ -1,12 +1,15 @@
 <?php
-require "dbDetails.php";
-
-$connectionString = "mysql:host=" . DBHOST . ";dbname=" . DBNAME;
-$dbuser = DBUSER;
-$dbpass = DBPASS;
+$host = 'cosc360.ok.ubc.ca/'; // Often this is 'localhost' or a specific server address or IP
+$dbname = 'db_18288647';
+$username = '18288647';
+$password = '18288647';
 
 try {
-    $pdo = new PDO($connectionString, $dbuser, $dbpass);
-} catch (\PDOException $e) {
-    throw new \PDOException($e->getMessage(), (int)$e->getCode());
+    $pdo = new PDO("mysql:host=$host;dbname=$dbname;charset=utf8mb4", $username, $password);
+    // Set the PDO error mode to exception
+    $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+    echo "Connected successfully"; 
+} catch(PDOException $e) {
+    echo "Connection failed: " . $e->getMessage();
 }
+?>
