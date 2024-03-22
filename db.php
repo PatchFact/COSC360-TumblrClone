34d3871341dabc488ab9fb838663301fa4c72bff
Jaidyn Gordon-Mason <jaidynrocks01@gmail.com>
2024-03-21 21:26:40 -0700

    blank

--- db.php
+++ db.php
@@ -1,10 +1,10 @@
 <?php
 require "dbDetails.php";
 
-$dbhost = DBHOST;
-$dbname = DBNAME;
-$dbuser = DBUSER;
-$dbpass = DBPASS;
+$dbhost = "cosc360.ok.ubc.ca";
+$dbname = "db_18288647";
+$dbuser = "18288647";
+$dbpass = "18288647";
 
 try {
     $pdo = new PDO("mysql:host=$dbhost;dbname=$dbname;charset=utf8mb4", $dbuser, $dbpass);
