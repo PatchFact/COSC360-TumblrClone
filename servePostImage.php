ec0cf5ed835742987c5992c8f90554be6f994ada
Jaidyn Gordon-Mason <jaidynrocks01@gmail.com>
2024-03-27 14:25:08 -0700

    fixed makePost, now trying to get picture to post

--- servePostImage.php
+++ servePostImage.php
@@ -5,7 +5,7 @@ $postId = isset($_GET['postId']) ? (int)$_GET['postId'] : 0;
 $imageData = Post::getImageSource($postId);
 
 if ($imageData) {
-    header('Content-Type: image/jpeg'); // Consider dynamically setting this based on stored image info
+    header('Content-Type: image/jpg'); // Consider dynamically setting this based on stored image info
     echo $imageData;
 } else {
     header("HTTP/1.1 404 Not Found");
