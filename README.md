3fc7e02f8b4cffaa74d43b5eb6c3ee9f9de8a1e7
Jaidyn Gordon-Mason <jaidynrocks01@gmail.com>
2024-03-27 17:19:40 -0700

    Added search post and search users to index.php

--- README.md
+++ README.md
@@ -86,7 +86,6 @@ This is the overall checklist needed for the project
 -   [x] Profile sidebar component
 -   [ ] Forgot password?
 -   [x] Routing errors/authorization
--   [ ] Configure database `my.ini` in deployment for profile pictures
 
 ### Katie
 
--- index.php
+++ index.php
@@ -6,9 +6,11 @@ require_once "head.php";
 require_once "user.php";
 require_once "post.php";
 
-$searchTerm = isset($_POST['search']) ? $_POST['search'] : null;
+$searchUserTerm = isset($_POST['searchUser']) ? $_POST['searchUser'] : "";
+$searchPostTerm = isset($_POST['searchPost']) ? $_POST['searchPost'] : null;
 
-$posts = $searchTerm ? Post::searchByKeyword($searchTerm) : Post::fetchAll();
+$users = $searchUserTerm ? User::searchByUsername($searchUserTerm) : User::fetchAll();
+$posts = $searchPostTerm ? Post::searchByKeyword($searchPostTerm) : Post::fetchAll();
 ?>
 
 <body>
@@ -18,24 +20,59 @@ $posts = $searchTerm ? Post::searchByKeyword($searchTerm) : Post::fetchAll();
             <div class="col-md-2">
                 <?php include "sidebarComponent.php"; ?>
             </div>
-            
             <div class="col-md-10">
                 <article id="feed">
                     <h2>Feed</h2>
                     <div class="row">
-                        <?php foreach ($posts as $post) : ?>
-                            <?php
-                            $user = User::getById($post->user_id);
-                            ?>
-                            <div class="col-9 mb-3">
+                        <div class="col-12 mb-3">
+                            <form method="POST" action="">
+                                <div class="input-group">
+                                    <input type="text" class="form-control" name="searchPost" placeholder="Search posts" value="<?php echo htmlspecialchars($searchPostTerm); ?>">
+                                    <div class="input-group-append">
+                                        <button class="btn btn-outline-secondary" type="submit">Search</button>
+                                    </div>
+                                </div>
+                            </form>
+                        </div>
+                        <div class="col-12 mb-3" style="max-height: 300px; overflow-y: scroll;">
+                            <?php $postCount = 0; ?>
+                            <?php foreach ($posts as $post) : ?>
+                                <?php
+                                $user = User::getById($post->user_id);
+                                ?>
                                 <a href="userPost.php?postId=<?php echo $post->post_id; ?>" class="text-decoration-none">
                                     <div class="d-flex align-items-center p-2 border rounded bg-light">
                                         <img src="serveProfilePic.php?userId=<?php echo $user->user_id ?>" alt="User Profile" class="mr-3" style="width: 50px; height: 50px; border-radius: 50%;">
                                         <h3 class="mb-0 text-dark"><?php echo htmlspecialchars($post->title); ?></h3>
                                     </div>
                                 </a>
-                            </div>
-                        <?php endforeach; ?>
+                                <?php $postCount++; ?>
+                                <?php if ($postCount >= 5) break; ?>
+                            <?php endforeach; ?>
+                        </div>
+                        <div class="col-12 mb-3">
+                            <form method="POST" action="">
+                                <div class="input-group">
+                                    <input type="text" class="form-control" name="searchUser" placeholder="Search users" value="<?php echo htmlspecialchars($searchUserTerm); ?>">
+                                    <div class="input-group-append">
+                                        <button class="btn btn-outline-secondary" type="submit">Search</button>
+                                    </div>
+                                </div>
+                            </form>
+                        </div>
+                        <div class="col-12 mb-3" style="max-height: 300px; overflow-y: scroll;">
+                            <?php $userCount = 0; ?>
+                            <?php foreach ($users as $user) : ?>
+                                <a href="profile.php?userId=<?php echo $user->user_id; ?>" class="text-decoration-none">
+                                    <div class="d-flex align-items-center p-2 border rounded bg-light">
+                                        <img src="serveProfilePic.php?userId=<?php echo $user->user_id ?>" alt="User Profile" class="mr-3" style="width: 50px; height: 50px; border-radius: 50%;">
+                                        <h3 class="mb-0 text-dark"><?php echo htmlspecialchars($user->username); ?></h3>
+                                    </div>
+                                </a>
+                                <?php $userCount++; ?>
+                                <?php if ($userCount >= 5) break; ?>
+                            <?php endforeach; ?>
+                        </div>
                     </div>
                 </article>
             </div>
